ec79d60e5e86f017668e298a6bf7b533a5ce3d5d
momiincx <nicha8280@gmail.com>
2025-03-06 16:07:46 +0700

    Script enginepower

---
+++ Assets/Script/Airplane.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float LiftBooster = 0.7f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrang = 0.01f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+
+        Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * LiftBooster);
+    }
+}
